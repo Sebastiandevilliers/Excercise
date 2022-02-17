5518c1bd01ea12b67ec61d9d83436119c2105258
Frank <frank.devilliers@yandex.com>
2022-02-17 22:02:45 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+
+function printCon(){
+    const word = [];
+    const tree = document.getElementById('text').value;
+    alert(tree);
+    
+    var size = tree.length;
+    alert(size);
+    for(i = size; i>=0; i--){
+        for(j = 0; j<size; j++){
+        car [j] = tree.charAt(i);
+        console.log(car[j]);
+        alert(car[j]);
+        }
+
+    }
+
+    alert(car[1]);
+
+    
+}
+
+function answer(){
+   
+  
+
+}
+
+answer();
